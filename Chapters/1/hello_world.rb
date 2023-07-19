0b89df9546cdb8c09d5a54e151e279fbd8d2be96
Marco Ferraro <mantofer2000@gmail.com>
2023-07-18 23:02:23 -0600

    adding hellos

---
+++ Chapters/1/hello_world.rb
@@ -0,0 +1,18 @@
+# Eveything in Ruby return something
+puts "Hello World"
+# P returns the argument, that is why in console there are ""
+p "Hello World"
+# print does not put a new line
+print "Hello "
+print "Marco \n"
+
+greeting = "Good Morning"
+puts greeting
+
+
+#function
+def say_hello(message)
+  puts message
+end
+
+say_hello "Hi Marco"
